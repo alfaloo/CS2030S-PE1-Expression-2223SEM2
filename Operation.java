c85c8fe43eddb92cad9f6ab79ace5824d38a6687
Zhiyang Lu <103179313+alfaloo@users.noreply.github.com>
2023-05-01 14:40:10 +0800

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -4,3 +4,24 @@
  *
  * @author A0000000X
  */
+
+abstract class Operation {
+
+  public static Operand of(char c, Operation op1, Operation op2) {
+      if (c == '*') {
+        Multiplication o =  new Multiplication(op1, op2);
+        return o.eval();
+      } else if (c == '+') {
+        Concatenation o =  new Concatenation(op1, op2);
+        return o.eval();
+      } else if (c == '^') {
+        XOR o =  new XOR(op1, op2);
+        return o.eval();
+      } else {
+        return null;
+      }
+  }
+
+  public abstract Object eval();
+
+}
